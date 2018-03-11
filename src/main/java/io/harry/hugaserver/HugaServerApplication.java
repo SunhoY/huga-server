600067ec57fc4d31391cb84557f5b7c1e1ccd521
sunho-yang <sunho.yang@linecorp.com>
2018-03-11 19:41:00 +0900

    first commit

---
+++ src/main/java/io/harry/hugaserver/HugaServerApplication.java
@@ -0,0 +1,12 @@
+package io.harry.hugaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HugaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HugaServerApplication.class, args);
+	}
+}
---
+++ src/test/java/io/harry/hugaserver/HugaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package io.harry.hugaserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HugaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
